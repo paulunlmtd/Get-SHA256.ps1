d9b3bb4ed2559bd1619ba10e221dfc681d871a21
paulunlmtd <paul@packetdriven.com>
2020-07-29 10:52:04 -0700

    Add files via upload

---
+++ Get-SHA256.ps1
@@ -0,0 +1,18 @@
+﻿#Prompt for manual entry of file location
+#$File = Read-Host -Prompt 'Enter file locaion'
+
+#Open file dialog box for location
+$File = Get-FileName
+$(Get-FileHash $File -Algorithm SHA256).Hash
+
+function Get-FileName($initialDirectory)
+{   
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+    Out-Null
+
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
